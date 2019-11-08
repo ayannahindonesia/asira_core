a931c31f5df1780ba96e95f2ce2b7ad55fcb1f12
albertjulian-Ayannah <ajulian@ayannah.com>
2019-11-08 16:04:30 +0700

    noDataCheckBox

--- src/components/subComponent/CheckBox.js
+++ src/components/subComponent/CheckBox.js
@@ -98,7 +98,9 @@ class CheckBoxClass extends React.Component {
                       </Grid>
                     )  
                   } 
-                }, data)
+
+                  return 'No Data'
+                }, this)
               }
             </Grid>
           </FormGroup>
@@ -159,7 +161,7 @@ class CheckBoxClass extends React.Component {
                     </Grid>
                   )
                 }
-              }, data)
+              }, this)
             }
 
             </FormGroup>
