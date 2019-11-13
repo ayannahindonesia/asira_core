ba584b788210a9ce56f19e9bd11972c4fbd4ca06
Aldi Hardiansyah <53806974+hard77@users.noreply.github.com>
2019-11-13 14:38:30 +0700

    Update url.js

--- src/components/url.js
+++ src/components/url.js
@@ -2,13 +2,13 @@
 //  export const serverUrlBorrower="https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/"
 
 export const serverUrl=window.location.origin.includes('staging') ? 
-"http://staging.ayannah.co.id/api-lender/" :
+"https://staging.ayannah.co.id/api-lender/" :
 "https://cors-anywhere.herokuapp.com/http://staging.ayannah.co.id/api-lender/";
 
 export const serverUrlBorrower=window.location.origin.includes('staging') ? 
-"http://staging.ayannah.co.id/api-borrower/" :
+"https://staging.ayannah.co.id/api-borrower/" :
 "https://cors-anywhere.herokuapp.com/http://staging.ayannah.co.id/api-borrower/";
 
 export const serverUrlGeo=window.location.origin.includes('staging') ? 
-"http://staging.ayannah.co.id/api-geomapping/" :
+"https://staging.ayannah.co.id/api-geomapping/" :
 "https://cors-anywhere.herokuapp.com/http://staging.ayannah.co.id/api-geomapping/"
