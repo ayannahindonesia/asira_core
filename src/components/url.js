f4ad25814dc1d5b86ed1ba510ee35d409b0e9695
Albert J <55692015+albertjulian-Ayannah@users.noreply.github.com>
2020-01-23 16:50:04 +0700

    Update url.js

--- src/components/url.js
+++ src/components/url.js
@@ -2,14 +2,14 @@
 //  export const serverUrlBorrower="https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/"
 
 
-export const serverUrl=window.location.origin.includes('asira') ? 
-"http://asira.ayannah.com/api-lender/" :
-"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-lender/";
+export const serverUrl=window.location.origin.includes('staging') ? 
+"https://staging.ayannah.co.id/api-lender/" :
+"https://cors-anywhere.herokuapp.com/https://staging.ayannah.co.id/api-lender/";
 
-export const serverUrlBorrower=window.location.origin.includes('asira') ? 
-"https://asira.ayannah.com/api-borrower/" :
-"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/";
+export const serverUrlBorrower=window.location.origin.includes('staging') ? 
+"https://staging.ayannah.co.id/api-borrower/" :
+"https://cors-anywhere.herokuapp.com/https://staging.ayannah.co.id/api-borrower/";
 
-export const serverUrlGeo=window.location.origin.includes('asira') ? 
-"http://asira.ayannah.com/api-geomapping/" :
-"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-geomapping/"
+export const serverUrlGeo=window.location.origin.includes('staging') ? 
+"https://staging.ayannah.com/api-geomapping/" :
+"https://cors-anywhere.herokuapp.com/https://staging.ayannah.co.id/api-geomapping/"
